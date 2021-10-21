faa0ba3ea2ec2c9bd6e698dc19381b7fa30a2c5c
MicaioValente <micaiovalente@gmail.com>
2021-10-21 10:20:21 -0300

    feat(yarn add uuid)

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import express from 'express'
-
+import { categoriesRoutes } from './routes/categories.routes'
 const app = express()
 
+app.use(express.json())
 
-app.listen(3333, () => console.log('server'))
+app.use(categoriesRoutes)
+
+app.listen(3333, () => console.log('boa '))
